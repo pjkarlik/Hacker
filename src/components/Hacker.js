ae6e4cfb4278f90908591dccf882bf4a2ebd31b3
Paul Karlik <pjkarlik@hotmail.com>
2016-03-22 18:43:27 -0400

    initial strobe

--- src/components/Hacker.js
+++ src/components/Hacker.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import { resolve } from './utils/styles';
 import HackerStyles from './Hacker.less';
+/**
+    ▄▄▄█████▓ ██░ ██ ▓█████     ██░ ██  ▄▄▄       ▄████▄   ██ ▄█▀▓█████  ██▀███
+    ▓  ██▒ ▓▒▓██░ ██▒▓█   ▀    ▓██░ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒
+    ▒ ▓██░ ▒░▒██▀▀██░▒███      ▒██▀▀██░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒
+    ░ ▓██▓ ░ ░▓█ ░██ ▒▓█  ▄    ░▓█ ░██ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒███▀▀█▄
+      ▒██▒ ░ ░▓█▒░██▓░▒████▒   ░▓█▒░██▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒
+      ▒ ░░    ▒ ░░▒░▒░░ ▒░ ░    ▒ ░░▒░▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░
+        ░     ▒ ░▒░ ░ ░ ░  ░    ▒ ░▒░ ░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░
+      ░       ░  ░░ ░   ░       ░  ░░ ░  ░   ▒   ░        ░ ░░ ░    ░     ░░   ░
+              ░  ░  ░   ░  ░    ░  ░  ░      ░  ░░ ░      ░  ░      ░  ░   ░
+                                                 ░
 
+      the Hacker is an experimental website in design and user interaction.
+      intended for entertainment purposes only and not associated
+      with any black-hat organization.
+*/
 export default class Hacker extends React.Component {
   static displayName = 'Hacker';
   static propTypes = {
@@ -12,14 +27,31 @@ export default class Hacker extends React.Component {
   };
   constructor(props) {
     super(props);
+    this.state = {
+      alt: false
+    };
+    this.negativeState = this.negativeState.bind(this);
   }
 
+  componentDidMount() {
+    this.interval = setInterval(this.negativeState, 100);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  negativeState() {
+    this.setState({
+      alt: !this.state.alt
+    });
+  }
   render() {
     return (
-      <div>
-        <div {...resolve(this.props, 'bodyContainer')}>
-          <h1>The HACKER</h1>
-        </div>
+      <div {...resolve(this.props, 'bodyContainer',
+        this.state.alt ? 'negaitve' : null)}>
+        <div {...resolve(this.props, 'boxEffect',
+          this.state.alt ? 'negaitve' : null)} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hacker from './components/Hacker';
-
+/**
+  Entry Point JavaScript
+*/
 if (typeof document !== 'undefined') {
   ReactDOM.render(<Hacker />, document.getElementById('outlet')); //
 }
