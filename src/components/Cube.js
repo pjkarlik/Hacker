78c684a6ea404257e2dceed27e1dbd6eb5516034
Paul Karlik <pjkarlik@hotmail.com>
2016-03-25 18:19:24 -0400

    merge hacker and cube

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -8,18 +8,27 @@ export default class Hacker extends React.Component {
   static displayName = 'Hacker';
   static propTypes = {
     rotation: React.PropTypes.array,
+    alt: React.PropTypes.bool,
     classes: React.PropTypes.object
   };
 
   constructor(props) {
     super(props);
     this.state = {
-      rotation: [0, 0]
+      rotation: [0, 0],
+      alt: this.props.alt
     };
     this.reactMouse = this.reactMouse.bind(this);
   }
 
   componentDidMount() {
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.alt !== this.props.alt) {
+      this.setState({
+        alt: !this.props.alt
+      });
+    }
   }
   reactMouse(e) {
     const rotation = [];
@@ -42,12 +51,12 @@ export default class Hacker extends React.Component {
       <div {...resolve(this.props, 'container')}
         onMouseMove = {this.reactMouse}>
         <div {...resolve(this.props, 'cube')} style = {styleObject}>
-          <div {...resolve(this.props, 'front')}>H</div>
-          <div {...resolve(this.props, 'back')}>C</div>
-          <div {...resolve(this.props, 'right')}>A</div>
-          <div {...resolve(this.props, 'left')}>K</div>
-          <div {...resolve(this.props, 'top')}>E</div>
-          <div {...resolve(this.props, 'bottom')}>R</div>
+          <div {...resolve(this.props, 'front', this.state.alt ? 'negaitve' : null)}>J</div>
+          <div {...resolve(this.props, 'back', this.state.alt ? 'negaitve' : null)}>&nbsp;</div>
+          <div {...resolve(this.props, 'right', this.state.alt ? 'negaitve' : null)}>K</div>
+          <div {...resolve(this.props, 'left', this.state.alt ? 'negaitve' : null)}>P</div>
+          <div {...resolve(this.props, 'top', this.state.alt ? 'negaitve' : null)}>&nbsp;</div>
+          <div {...resolve(this.props, 'bottom', this.state.alt ? 'negaitve' : null)}>&nbsp;</div>
         </div>
       </div>
     );
--- src/components/Hacker.js
+++ src/components/Hacker.js
@@ -34,14 +34,40 @@ export default class Hacker extends React.Component {
       type: true,
       panelStyle: null
     };
+    this.negativeState = this.negativeState.bind(this);
+    this.typeState = this.typeState.bind(this);
+  }
+  componentDidMount() {
+    this.interval = setInterval(this.negativeState, 100);
+    // the hacker frequencey //
+    this.interval = setInterval(this.typeState, 2600);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  negativeState() {
+    this.setState({
+      alt: !this.state.alt
+    });
+  }
+  typeState() {
+    this.setState({
+      type: !this.state.type
+    });
+  }
   render() {
+    const background = (
+      <div {...resolve(this.props,
+        this.state.type ? 'boxEffect' : 'circleEffect',
+        this.state.alt ? 'negaitve' : null)} />
+    );
     return (
       <div {...resolve(this.props, 'bodyContainer',
         this.state.alt ? 'negaitve' : null)}>
-
+        {background}
         <Cube classes = {CubeStyles}
+          alt = {this.state.alt}
           rotation = {this.state.rotation} />
 
       </div>
